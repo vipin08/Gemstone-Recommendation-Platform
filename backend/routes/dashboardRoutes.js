3be50b324148511d3e4deeaeefa1cbc8231ba81f
vipinsohal <strom4614@gmail.com>
2026-06-13 17:18:21 +0530

    first commit

--- backend/routes/dashboardRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { getDashboardStatsHandler } from "../controllers/dashboardController.js"
-
-const router = Router()
-
-router.get("/dashboard/stats", getDashboardStatsHandler)
-
-export default router
